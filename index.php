76538ac5d66d971cde4aa370bd3a7d7bf969b8cb
PracaOl <oliwer.pietrzycki@op.pl>
2023-09-18 09:37:03 +0200

    Backup

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl_1.css">
+    <title>Wędkowanie</title>
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <div id="Lewo1">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+            
+        </ol>
+    </div>
+    <div id="Lewo2">
+        <h3>Ryby drapieżne naszych wód</h3>
+        <table>
+            <tr>
+                <th>L.p.</th>
+                <th>Gatunek</th>
+                <th>Występowanie</th>
+            </tr>
+        </table>
+    </div>
+    <div id="Prawo">
+        <img src="pliki1/ryba1.jpg" alt="Sum">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <footer>
+        <p>Stonę wykonał: 13</p>
+    </footer>
+</body>
+</html>
